43d76e009ddffd38500a55899d39d5c83ec9dbb0
Dote10 <zoqol@daum.net>
2024-01-15 12:23:40 +0900

    docker로 실행한 Mysql 외부 노출 포트 번호 수정

--- docker-in-aws/api/db.js
+++ docker-in-aws/api/db.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit:10,
     host:'app_db',
+    prot:3307,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:'multi'
